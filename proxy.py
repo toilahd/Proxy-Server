6229485114ea74218bac306c8a7a807f31ccf553
Huy Quang Nguyen <116057557+toilahd@users.noreply.github.com>
2023-08-17 20:48:46 +0700

    Comment code

--- proxy.py
+++ proxy.py
@@ -6,23 +6,30 @@ SUPPORTED_METHODS = ['GET', 'POST', 'HEAD']
 
 
 def send_error(conn):
-    
     with open("error403.html", 'r') as f:
         resdata = f.read()
     conn.send(b'HTTP/1.1 403 Forbidden\r\nContent-Type: text/html\r\n\r\n' + resdata.encode('ISO-8859-1'))
 
 def extract_msg(msg):
-    method = msg.split()[0] #extract the method from the message
-    url = msg.split()[1] #extract the url from the message
+    #Trích method
+    method = msg.split()[0]
+
+    #Trích url
+    url = msg.split()[1] 
+
+    #Từ url trích ra domain + filepath
     if (url.find("://") != -1):
         url = url.partition("://")[2]
     domain, punk, file_path = url.partition("/")
     file_path = "/" + file_path
     return method, domain, file_path
 
+#Tạo request để gửi cho Web Server
 def handle_requests(msg):
     method, domain, file_path = extract_msg(msg)
     request = f"{method} {file_path} HTTP/1.1\r\n"
+
+    #Thêm Connection: close để tự ngắt kết nối khi đã nhận đủ dữ liệu 
     if method == "POST":
         if msg.find("Connection: ") != -1:
             request += msg.partition("\r\n")[2].partition("Connection: ")[0] + "Connection: close\r\n" + msg.partition("Connection: ")[2].partition("\r\n")[2]
@@ -33,41 +40,57 @@ def handle_requests(msg):
         request += f"Connection: close\r\n\r\n"
     return request
 def handle_Client(tcpClient, addr):
-    #msg receiving from client
-    msg = tcpClient.recv(4096).decode("ISO-8859-1")
-    #extract the info we need from the given message
+
+    #message nhận được từ Client
+    msg = tcpClient.recv(4096)
     
     if not msg:
         return
     try: 
-        #print(f"Request: {addr}\n{msg}\r\n")
         print(f"Receive connection from: {addr}\r\n")
     except:
         tcpClient.close()
         return
     
+    
+    if msg.find(b'Accept-Encoding:') != -1:
+        msg = msg.replace(msg[msg.find(b'Accept-Encoding:'):].split(b'\r\n')[0] + b'\r\n', b'')
+    #Decode
+    msg = msg.decode('ISO-8859-1')
+    #Trích xuất method, domain và file_path từ msg 
     method, domain, file_path = extract_msg(msg)
+
+    #Nếu method nằm trong SUPPORTED_METHODS thì tạo request
     if method in SUPPORTED_METHODS:
         request = handle_requests(msg) 
     else:
-        send_error()
+        #Gửi lỗi 403 
+        send_error(tcpClient)
         return
     
+    #Tạo socket cho Webserver, kết nối với port 80
     server = socket(AF_INET, SOCK_STREAM)
     server.connect((domain, 80))
+
+    #In request và gửi request đã encode
     print(request)
     server.send(request.encode())
+
+    #Bắt đầu nhận response
     response = b""
     while True:
         chunk = server.recv(4096) #config later
         if len(chunk) == 0:
             break
         response += chunk
+
+    #In ra response, sẽ làm thành một hàm sau 
     try:
         print(response.decode())
     except:
         print("Cannot decode")
 
+    #Gửi response về cho Client
     tcpClient.sendall(response)
 
 if len(sys.argv) <= 1:
