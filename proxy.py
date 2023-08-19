307481c7df91e2e55fe36242a750ce4c59bde87c
Huy Quang Nguyen <116057557+toilahd@users.noreply.github.com>
2023-08-19 15:12:49 +0700

    Final

--- proxy.py
+++ proxy.py
@@ -12,40 +12,20 @@ def get_configs():
     configs = json.load(file_name)
     return configs['supported_methods'], configs['cache_time'], configs['whitelist_enabled'], configs['white_list'], configs['time_restriction'], configs['time_allow'], configs['decode_format'], configs['supported_img_formats']
 supported_methods, cache_time, whitelist_enabled, white_list, time_restriction, time_allow, decode_format, supported_img_formats = get_configs()
-start_time, end_time = map(int, time_allow.split('-'))
 
-def send_error(conn):
+def send403(conn):
     with open("403.html", 'r') as f:
         resdata = f.read()
     conn.send(b'HTTP/1.1 403 Forbidden\r\nContent-Type: text/html\r\n\r\n' + resdata.encode('ISO-8859-1'))
+    print("Send HTTP Error 403 Forbidden")
 
 def respond_client(response):
-    data = response.split("\r\n\r\n")[0]
+    data = response.decode().split('\r\n')[0]
     try:
-        print(data.decode())
+        print("Response received: ")
+        print(data + '\r\n')
     except:
         print("Cannot decode")
-        print("Its an image!")
-
-def extract_msg(msg):
-    #Trích method
-    method = msg.split()[0]
-    #Trích url
-    url = msg.split()[1] 
-    #Từ url trích ra domain + filepath
-    if (url.find("://") != -1):
-        url = url.partition("://")[2]
-    domain, punk, file_path = url.partition("/")
-    file_path = "/" + file_path
-    return method, domain, file_path
-
-def respond_client(response):
-    response
-    try:
-        print(response.decode())
-    except:
-        print("Cannot decode")
-        print(response)
 
 
 def extract_msg(msg):
@@ -60,6 +40,18 @@ def extract_msg(msg):
     file_path = "/" + file_path
     return method, domain, file_path
 
+def check_time(img, img_header):
+    # Tìm thời gian hiện tại
+    current_time = datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=None)
+    # Lấy nguồn chứa thời gian ảnh từ header
+    img_time_src = img_header.decode(decode_format).partition("Date: ")[2].partition(" GMT")[0].partition(", ")[2]
+    # Lấy thời gian ảnh
+    img_time = datetime.datetime.strptime(img_time_src, "%d %b %Y %H:%M:%S")
+
+    # Kiểm tra nếu ảnh đã lưu quá <cache_time> (thời gian tối đa cho phép lưu giữ ảnh trong cache)
+    if (current_time >= img_time + datetime.timedelta(seconds = int(cache_time))):
+        return False, ""
+    return True, img_header + b"\r\n\r\n" + img
 
 def getCachedImage(message):
     method, webServer, file = extract_msg(message)
@@ -88,6 +80,34 @@ def getCachedImage(message):
         return False, ""
     return True, imgHeader + b"\r\n\r\n" + img
 
+def ImgFromCache(msg):
+    method, domain, file_path = extract_msg(msg)
+    # If does not request image or image type not supported
+    fileEx = file_path.split("/").pop().partition(".")[2]
+    if fileEx not in supported_img_formats:
+        return False, ""
+    # Get the image and image header path
+    img_path = f"{os.getcwd()}/cache/{domain}{file_path}"
+    img_header_path = img_path[:img_path.rfind(".")] + ".bin"
+    # If the image is cached
+    try:
+        with open(img_path, "rb") as file:
+            img = file.read()
+        with open(img_header_path, "rb") as file:
+            img_header = file.read()
+    except:
+        return False, ""
+    
+    # Kiểm tra điều kiện thời gian
+    return check_time(img, img_header)
+
+def saveImg(img, img_path, img_header, img_header_path):
+    # Save image and header to cache
+    with open(img_path, "wb") as file:
+        file.write(img.encode(decode_format))
+    with open(img_header_path, "wb") as file:
+        file.write(img_header.encode(decode_format))
+    return
 
 def saveImageToCache(message, webReply):
     method, webServer, file = extract_msg(message)
@@ -115,17 +135,45 @@ def saveImageToCache(message, webReply):
         
     return
 
+def ImgToCache(msg, response):
+
+    method, domain, file_path = extract_msg(msg)
+
+    # If does not request image or image type not supported
+    fileEx = file_path.split("/").pop().partition(".")[2]
+    if fileEx not in supported_img_formats:
+        return
+    
+    # Get the path of the image, header and the folder containing them
+    img_path = f"{os.getcwd()}/cache/{domain}{file_path}"
+    img_header_path = img_path[:img_path.rfind(".")] + ".bin"
+    folder_path = img_path[:img_path.rfind("/")]
+
+    # If the folder does not exist, create that folder
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    img_header, trash, img = response.decode(decode_format).partition("\r\n\r\n")
+    saveImg(img, img_path, img_header, img_header_path)
+    return
+
 
 #Tạo request để gửi cho Web Server
 def handle_requests(msg):
-    # If is cached
-    status, cachedReply = getCachedImage(msg)
+    #Nếu đã cached
+    status, cachedReply = ImgFromCache(msg)
     if status == True:
         print("\r\nGET FROM CACHE SUCCESSFULLY\r\n")
         return cachedReply
-    method, domain, file_path = extract_msg(msg)
-    request = f"{method} {file_path} HTTP/1.1\r\n"
+    
+    #Nếu chưa cached
+    method, web_server, file_path = extract_msg(msg)
 
+    #Tạo socket cho web server
+    host = socket(AF_INET, SOCK_STREAM)
+    #Ket noi toi web server voi port 80
+    host.connect((web_server, 80))
+
+    request = f"{method} {file_path} HTTP/1.1\r\n"
     #Thêm Connection: close để tự ngắt kết nối khi đã nhận đủ dữ liệu 
     if method == "POST":
         if msg.find("Connection: ") != -1:
@@ -133,103 +181,123 @@ def handle_requests(msg):
         else:
             request += msg.partition("\r\n\r\n")[0] + "\r\nConnection: close\r\n\r\n" + msg.partition("\r\n\r\n")[2]
     else:
-        request += f"Host: {domain}\r\n"
+        request += f"Host: {web_server}\r\n"
         request += f"Connection: close\r\n\r\n"
-    
-    #Tạo socket cho Webserver, kết nối với port 80
-    server = socket(AF_INET, SOCK_STREAM)
-    server.connect((domain, 80))
-
+        
     #In request và gửi request đã encode
-    
-    print(f"Request from client to server: \r\n{request}")
-    server.send(request.encode())
+    print(f"Sending request: \r\n{request}")
+    host.send(request.encode())
 
     #Bắt đầu nhận response
     response = b""
     while True:
-        chunk = server.recv(4096) #config later
+        chunk = host.recv(4096)
         if len(chunk) == 0:
             break
         response += chunk
-    saveImageToCache(msg, response)
-    server.close()
+
+    # Nhét img vào cache
+    ImgToCache(msg, response)
+    host.close()
+
     return response
 
-
 def handle_Client(tcpClient, addr):
-    #message nhận được từ Client
-    msg = tcpClient.recv(4096)   
-    #Nếu bật time restriction và đang trong khoảng thời gian không cho phép
-    if time_restriction:
-        if (time(start_time,0,0) < datetime.now().time() < time(end_time,0,0)):
-            print("Not in available time")
-            send_error(tcpClient)
-            tcpClient.close()
-            return 
-
+    
+    print(f"<><><><><><><><><><><><><><><><><><><><><><>\r\n") 
+     
+    try:
+        msg = tcpClient.recv(4096)
+    except:
+        msg = ""
+  
     if not msg:
+        print("No message from client\r\n")
+        tcpClient.close()
         return
+
     try: 
-        print(f"<><><><><><><><><><><><><><><><><><><><><><>")
         print(f"Receive connection from: {addr}\r\n")
     except:
         tcpClient.close()
         return
     
-    
     if msg.find(b'Accept-Encoding:') != -1:
         msg = msg.replace(msg[msg.find(b'Accept-Encoding:'):].split(b'\r\n')[0] + b'\r\n', b'')
-    #Decode
-    msg = msg.decode('ISO-8859-1')
+
+    #Nếu bật time-restriction và đang nằm trong khoảng thời gian không cho phép thì gửi mã 403
+    start_time, end_time = map(int, time_allow.split('-'))    
+    if time_restriction and (time(start_time,0,0) < datetime.now().time() < time(end_time,0,0)):
+        print("Not in available time!")
+        send403(tcpClient)
+        tcpClient.close()
+        return 
+    
+    #nếu bật whitelist và domain không nằm trong whitelist thì gửi mã 403
+    if whitelist_enabled and domain not in white_list: 
+        print("This domain is not allowed to access!")
+        send403(tcpClient)
+        tcpClient.close()
+        return  
+
+    #Decode message 
+    msg = msg.decode(decode_format)
     #Trích xuất method, domain và file_path từ msg 
     method, domain, file_path = extract_msg(msg)
-    if whitelist_enabled:
-        if domain not in white_list:
-            print("Not in whitelist!")
-            send_error(tcpClient)
-            tcpClient.close()
-            return 
+    
 
-    #Nếu method nằm trong SUPPORTED_METHODS thì tạo request
+    #Nếu method nằm trong SUPPORTED_METHODS thì bắt đầu tạo resquest và gửi
     if method in supported_methods:
         response = handle_requests(msg) 
     else:
-        #Gửi lỗi 403 
-        send_error(tcpClient)
+    #Nếu không thì trả về mã 403 
+        print("Unsupported method:" + method)
+        send403(tcpClient)
         tcpClient.close()
         return
     
-    #In ra response, sẽ làm thành một hàm sau 
-    
-    #respond_client(response)
+    #In ra response
+    respond_client(response)
 
     #Gửi response về cho Client
     tcpClient.sendall(response)
     tcpClient.close()
 
-if len(sys.argv) <= 1:
-    print('Usage: "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server]')
-    sys.exit(2)
 
-# Create a server socket, bind it to a port and start listening
-server = socket(AF_INET, SOCK_STREAM)
-# Re-use the socket   
-server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
-HOST = sys.argv[1].split(':')[0]
-PORT = int(sys.argv[1].split(':')[1])
+def main():
+    if len(sys.argv) <= 1:
+        print('Usage: "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server]')
+        sys.exit(2)
+    
+    print(f"cache_time: {cache_time}")
+    print(f"whitelist: {white_list}")
+    print(f"time_forbid: {time_allow}")
 
-# become a server socket
-server.bind((HOST, PORT))
-server.listen(10)
-print(f'Proxy server is listening on {HOST}:{PORT}')
+    # Create a server socket, bind it to a port and start listening
+    # Re-use the socket   
+    server = socket(AF_INET, SOCK_STREAM)
+    server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
-while True: 
-   #Start receiving data from the client
-   print('Ready to serve...')
-   tcpClient, addr = server.accept()
-   thread = threading.Thread(target=handle_Client, args=(tcpClient, addr))
-   #thread.setDaemon(True)
-   #Daemon threads are threads that run in the background and are terminated when the main program exits. 
-   thread.start()
+    HOST = sys.argv[1].split(':')[0]
+    PORT = int(sys.argv[1].split(':')[1])
+
+    # become a server socket
+    server.bind((HOST, PORT))
+    server.listen(3)
+    print(f'Proxy server is listening on {HOST}:{PORT}')
+
+    while True:
+        try:
+            #Start receiving data from the client
+            print('Ready to serve...')
+            tcpClient, addr = server.accept()
+
+            thread = threading.Thread(target=handle_Client, args=(tcpClient, addr))
+            #thread.setDaemon(True)
+            thread.start()
+        except KeyboardInterrupt:
+            print("Closing program")
+            tcpClient.close()
+if __name__ == "__main__":
+	main()
